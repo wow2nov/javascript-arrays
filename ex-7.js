82385c2186314576e57e0b6e8f6bec2204275368
Supapond <supapondmaliwab@gmail.com>
2025-09-17 14:21:20 +0700

    fead: ex-7 add code orders

--- ex-7.js
+++ ex-7.js
@@ -42,7 +42,22 @@ let orders = [
 ];
 
 // Start coding here
-let isArray;
-let creditCardTypeOfBlindermann;
-let productQuantityOfJoannet;
-let totalPurchaseOfDary;
+let isArray = (Array.isArray(orders));
+console.log(isArray);
+
+let creditCardTypeOfBlindermann = (orders[2].creditCardType);
+console.log(creditCardTypeOfBlindermann);
+
+orders[2].creditCardType = "visa";
+console.log(orders[2].creditCardType);
+
+
+let productQuantityOfJoannet = orders[3].productQuantity;
+console.log(productQuantityOfJoannet);
+
+
+let totalPurchaseOfDary = orders[1].productPrice * orders[1].productQuantity;
+console.log(totalPurchaseOfDary);
+
+orders.shift();
+console.log(orders);
